7e69f634052e55184e2d4a0eb18d7c5d36dfc2df
Dean Coffman <deansdetail@gmail.com>
2023-04-22 16:24:22 -0400

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,18 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Dean Coffman";
+		int age = 36;
+		String state = "Iowa";
+
+		System.out.println("Name:" + name);
+		System.out.println("Age:" + age);
+		System.out.println("State:" + state);
+		
+
+
+
+	}
+
+
+
+}
