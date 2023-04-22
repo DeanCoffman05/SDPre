97485e9c0a12f233c18967a5ddf40762299edc29
Dean Coffman <deansdetail@gmail.com>
2023-04-22 19:11:59 -0400

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int Year_Born = 1986;
+		String Month_Born = "May";
+		Boolean Weekend = true; 
+
+		System.out.println(Year_Born);
+		System.out.println(Month_Born);
+		System.out.println(Weekend);
+
+	}
+
+
+}
