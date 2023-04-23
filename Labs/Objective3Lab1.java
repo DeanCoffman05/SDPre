da8211f1841cfffd567d46cb7235cf8f03aa6df2
Dean Coffman <deansdetail@gmail.com>
2023-04-22 20:05:24 -0400

    Obejctive3Experimentation and Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,20 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1, num2, num3, average;
+
+		num1 = 5.9;
+		num2 = 55.9;
+		num3 = 105.9;
+
+		average = (num1 + num2 + num3 / 3);
+
+		System.out.println(average);
+
+
+
+
+	}
+		
+
+
+}
