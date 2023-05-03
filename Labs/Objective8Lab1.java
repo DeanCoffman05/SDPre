f60ea7b277a558bf0942a0f994c470f0ec296f41
Dean Coffman <deansdetail@gmail.com>
2023-05-02 23:49:27 -0400

    Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,14 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			int total = i;
+			System.out.println(total);
+		}
+		
+		for (int i = 10; i >= 1; i--) {
+			int total = i;
+			System.out.println(total);
+		}
+	}
+}
